61d15005752a2cc1689eb933213ea1be85c96308
shinmink <sinmk99@hansung.ac.kr>
2025-05-22 14:43:03 +0900

    FEAT : LoginScreen.tsx, Si KeyboardAvoidingView 적용으로 키보드 토글 여백조정

--- front/src/config/apiConfig.ts
+++ front/src/config/apiConfig.ts
@@ -2,21 +2,24 @@
 import {Platform} from 'react-native';
 
 /** ✅ 공통 API 루트 URL */
-// 로컬 에뮬레이터로 돌릴때
+
+/** 로컬 에뮬레이터로 돌릴때 */
 // export const API_ROOT_URL =
 //   Platform.OS === 'android'
 //     ? 'http://10.0.2.2:8080/api'
 //     : 'http://localhost:8080/api';
 
-//export const API_ROOT_URL = '(배포된 서버 url)/api'; // 백엔드 API 서버 주소
+
+
+/** 배포된  ec2 서버로 돌릴때 */
+export const API_ROOT_URL = 'http://43.202.62.32:8080/api'; // 백엔드 API 서버 주소
 
 // ipconfig getifaddr en0
 // 172.29.66.96 ( 옥정 스벅 와이파이 )
 
 // 172.30.1.87 ( 집 와이파이 )
 
-
-export const API_ROOT_URL =
-  Platform.OS === 'android'
-    ? 'http://172.30.1.87:8080/api'
-    : 'http://172.30.1.87:8080/api';
+// export const API_ROOT_URL =
+//   Platform.OS === 'android'
+//     ? 'http://172.30.1.87:8080/api'
+//     : 'http://172.30.1.87:8080/api';
