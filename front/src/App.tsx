331a836484034d7aae0dbb0d78c9b0d507a21057
shinmink <sinmk99@hansung.ac.kr>
2025-05-28 23:49:46 +0900

    FEAT : 디버그 문구 강제 무시

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,15 @@ import AppNavigator from './navigation/AppNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 import Snackbar from './common/Snackbar';
 
+// App.tsx 최상단
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs();
+
+// 필요 시 에러 핸들러도 추가
+ErrorUtils.setGlobalHandler((error, isFatal) => {
+  console.log('❌ 무시된 에러:', error.message);
+});
+
 const App = () => {
     return (
         <SafeAreaProvider>
