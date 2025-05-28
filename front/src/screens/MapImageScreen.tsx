90a84b7c71cd4bca0d07527675a4fe465c3f4e16
shinmink <sinmk99@hansung.ac.kr>
2025-05-28 13:41:13 +0900

    FIX : 안드로이드 지도 방어

--- front/src/screens/MapImageScreen.tsx
+++ front/src/screens/MapImageScreen.tsx
@@ -8,7 +8,7 @@ const MapImageScreen = () => {
       <Image
         source={require('../assets/walk_placeholder.jpg')} // ✅ 본인이 넣은 이미지 경로로 교체
         style={styles.image}
-        resizeMode="cover" // 또는 'contain' 원할 경우
+        resizeMode="contain" // 또는 'contain' 원할 경우
       />
     </View>
   );
@@ -17,11 +17,11 @@ const MapImageScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000', // 이미지 로딩 중에도 깔끔하게 보이도록
+    backgroundColor: '#ffffff', // 이미지 로딩 중에도 깔끔하게 보이도록
   },
   image: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: '100%',
+    height: '110%',
   },
 });
 
